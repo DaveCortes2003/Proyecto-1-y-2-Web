07011a84842ef5e14515bc045a8ca18ed657666c
DaveCortes2003 <deivyduo@gmail.com>
2022-11-27 23:04:29 -0600

    register ended

--- proyecto2/js/register.js
+++ proyecto2/js/register.js
@@ -1,44 +1,82 @@
 'use strict';
 const userKey = 'users';
 function saveUser() {
-    
-	const name = document.getElementById('name').value;
+
+    let valor=verificar();
+	
+    if(valor == true){
+        const name = document.getElementById('name').value;
+        const lastName = document.getElementById('Lname').value;
+        const cellphone = document.getElementById('phone').value;
+        const username = document.getElementById('Username').value;
+        const pass = document.getElementById('pass').value;
+        const Rpass = document.getElementById('Rpass').value;
+        // add it to the database
+
+        const user = {
+            name,
+            lastName,
+            cellphone,
+            username,
+            pass,
+            Rpass
+        };
+        user.name=name;
+        user.lastName=lastName;
+        user.cellphone=cellphone;
+        user.username=username;
+        user.pass=pass;
+        user.Rpass=Rpass;
+
+        let users = JSON.parse(localStorage.getItem(userKey));
+
+        if (!users) {
+            users = [];
+        }
+
+        users.push(user);
+
+
+        localStorage.setItem(userKey, JSON.stringify(users));
+
+
+        clearFields();
+        window.location.href = '/proyecto2/html/login.html';
+    }
+}
+function verificar(){
+    let l =false;
+    const name = document.getElementById('name').value;
 	const lastName = document.getElementById('Lname').value;
     const cellphone = document.getElementById('phone').value;
     const username = document.getElementById('Username').value;
     const pass = document.getElementById('pass').value;
     const Rpass = document.getElementById('Rpass').value;
-	// add it to the database
-
-    const user = {
-		name,
-		lastName,
-        cellphone,
-        username,
-        pass,
-        Rpass
-	};
-    user.name=name;
-    user.lastName=lastName;
-    user.cellphone=cellphone;
-    user.username=username;
-    user.pass=pass;
-    user.Rpass=Rpass;
 
     let users = JSON.parse(localStorage.getItem(userKey));
 
     if (!users) {
         users = [];
-      }
+    }
 
-    users.push(user);
+    if(name=='' || lastName=='' || cellphone=='' || username=='' || pass=='' || Rpass==''){
+        window.alert("Por favor llene todos los espacios");
+    }
+    if(pass != Rpass){
+        window.alert("Las contraseñas no coinciden");
+    }
 
+    else{
+        l=true;
+        users.forEach((user)=>{
+            if(user.username == username){
+                window.alert("El username ya existe");
+                l=false;
+            }
+        });
+    }
 
-	localStorage.setItem(userKey, JSON.stringify(users));
-
-
-	clearFields();
-    window.location.href = '/proyecto2/html/login.html';
+    return l;
 }
 
 function clearFields() {
